3d57379140d57d89115ca840cf97768b9b4b7979
André <Andr@andre-note>
2014-08-09 02:17:12 -0300

    Internacionalização

--- WebApplication/src/main/java/br/com/formento/webApplication/bean/LanguageBean.java
+++ WebApplication/src/main/java/br/com/formento/webApplication/bean/LanguageBean.java
@@ -2,14 +2,11 @@ package br.com.formento.webApplication.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ValueChangeEvent;
 import javax.inject.Named;
 
 @Named
@@ -21,45 +18,27 @@ public class LanguageBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 7900302688122643039L;
 
-	private static Map<String, Locale> countriesInMap;
+	private static List<Locale> locales;
 
-	public static Map<String, Locale> getCountriesInMap() {
-		if (countriesInMap == null) {
-			countriesInMap = new LinkedHashMap<String, Locale>();
-			countriesInMap.put("English", Locale.ENGLISH); // label, value
-			Locale portuguese = new Locale("pt", "BR");
-			countriesInMap.put(portuguese.getCountry(), portuguese);
-		}
-		return countriesInMap;
+	static {
+		locales = new ArrayList<>();
+		locales.add(Locale.ENGLISH);
+		locales.add(new Locale("pt", "BR"));
 	}
-
-	private String localeCode;
 
 	public LanguageBean() {
 	}
 
 	public List<Locale> getLocales() {
-		List<Locale> arrayList = new ArrayList<Locale>(getCountriesInMap().values());
-		return arrayList;
+		return locales;
 	}
 
-	public String getLocaleCode() {
-		return localeCode;
+	public Locale getLocale() {
+		return FacesContext.getCurrentInstance().getViewRoot().getLocale();
 	}
 
-	public void setLocaleCode(String localeCode) {
-		this.localeCode = localeCode;
-	}
-
-	// value change event listener
-	public void countryLocaleCodeChanged(ValueChangeEvent e) {
-		String newLocaleValue = e.getNewValue().toString();
-		// loop country map to compare the locale code
-		for (Map.Entry<String, Locale> entry : getCountriesInMap().entrySet()) {
-			if (entry.getValue().toString().equals(newLocaleValue)) {
-				FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale) entry.getValue());
-			}
-		}
+	public void setLocale(Locale locale) {
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
 	}
 
 }
